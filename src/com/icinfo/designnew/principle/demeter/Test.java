48faaacefe1b4c3e029a6b604d21d1ef3335ef4b
jiakai908 <jiakai908@163.com>
2019-03-10 22:03:40 +0800

    add test类注释

--- src/com/icinfo/designnew/principle/demeter/Test.java
+++ src/com/icinfo/designnew/principle/demeter/Test.java
@@ -1,5 +1,10 @@
 package com.icinfo.designnew.principle.demeter;
 
+/**
+ *  描述
+ * @author jkk
+ * @date 2019年03月10日
+ */
 public class Test {
     public static void main(String[] args) {
         TeamLeader teamLeader = new TeamLeader();
